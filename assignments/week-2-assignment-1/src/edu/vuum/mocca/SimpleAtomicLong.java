4dfb4b9e4e177025608aa6935fa748f06e38d839
Zhlain <zhlain@gmail.com>
2014-06-01 22:46:53 +0200

    Assignment 2 solution
    Assignment 2 solution, passed tests

--- assignments/week-2-assignment-1/src/edu/vuum/mocca/SimpleAtomicLong.java
+++ assignments/week-2-assignment-1/src/edu/vuum/mocca/SimpleAtomicLong.java
@@ -1,106 +1,115 @@
 package edu.vuum.mocca;
 
 import java.util.concurrent.locks.ReentrantReadWriteLock;
-import java.util.concurrent.locks.Lock;
 
 /**
  * @class SimpleAtomicLong
- *
+ * 
  * @brief This class implements a subset of the
  *        java.util.concurrent.atomic.SimpleAtomicLong class using a
  *        ReentrantReadWriteLock to illustrate how they work.
  */
-class SimpleAtomicLong
-{
-    /**
-     * The value that's manipulated atomically via the methods.
-     */
-    private long mValue;
-    
-    /**
-     * The ReentrantReadWriteLock used to serialize access to mValue.
-     */
+class SimpleAtomicLong {
+	/**
+	 * The value that's manipulated atomically via the methods.
+	 */
+	private long mValue;
 
-    // TODO -- you fill in here by replacing the null with an
-    // initialization of ReentrantReadWriteLock.
-    private ReentrantReadWriteLock mRWLock = null;
+	/**
+	 * The ReentrantReadWriteLock used to serialize access to mValue.
+	 */
 
-    /**
-     * Creates a new SimpleAtomicLong with the given initial value.
-     */
-    public SimpleAtomicLong(long initialValue)
-    {
-        // TODO -- you fill in here
-    }
+	// TODO -- you fill in here by replacing the null with an
+	// initialization of ReentrantReadWriteLock.
+	private ReentrantReadWriteLock mRWLock = new ReentrantReadWriteLock();
 
-    /**
-     * @brief Gets the current value.
-     * 
-     * @returns The current value
-     */
-    public long get()
-    {
-        long value;
+	/**
+	 * Creates a new SimpleAtomicLong with the given initial value.
+	 */
+	public SimpleAtomicLong(long initialValue) {
+		// TODO -- you fill in here
+		mValue = initialValue;
+	}
 
-        // TODO -- you fill in here
+	/**
+	 * @brief Gets the current value.
+	 * 
+	 * @returns The current value
+	 */
+	public long get() {
+		long value;
 
-        return value;
-    }
+		// TODO -- you fill in here
+		value = this.mValue;
 
-    /**
-     * @brief Atomically decrements by one the current value
-     *
-     * @returns the updated value
-     */
-    public long decrementAndGet()
-    {
-        long value = 0;
+		return value;
+	}
 
-        // TODO -- you fill in here
+	/**
+	 * @brief Atomically decrements by one the current value
+	 * 
+	 * @returns the updated value
+	 */
+	public long decrementAndGet() {
+		long value = 0;
 
-        return value;
-    }
+		// TODO -- you fill in here
+		mRWLock.writeLock().lock();
+		this.mValue = this.mValue - 1;
+		value = this.mValue;
+		mRWLock.writeLock().unlock();
 
-    /**
-     * @brief Atomically increments by one the current value
-     *
-     * @returns the previous value
-     */
-    public long getAndIncrement()
-    {
-        long value = 0;
+		return value;
+	}
 
-        // TODO -- you fill in here
+	/**
+	 * @brief Atomically increments by one the current value
+	 * 
+	 * @returns the previous value
+	 */
+	public long getAndIncrement() {
+		long value = 0;
 
-        return value;
-    }
+		// TODO -- you fill in here
+		mRWLock.writeLock().lock();
+		value = this.mValue;
+		this.mValue = this.mValue + 1;
+		mRWLock.writeLock().unlock();
 
-    /**
-     * @brief Atomically decrements by one the current value
-     *
-     * @returns the previous value
-     */
-    public long getAndDecrement()
-    {
-        long value = 0;
+		return value;
+	}
 
-        // TODO -- you fill in here
+	/**
+	 * @brief Atomically decrements by one the current value
+	 * 
+	 * @returns the previous value
+	 */
+	public long getAndDecrement() {
+		long value = 0;
 
-        return value;
-    }
+		// TODO -- you fill in here
+		mRWLock.writeLock().lock();
+		value = this.mValue;
+		this.mValue = this.mValue - 1;
+		mRWLock.writeLock().unlock();
 
-    /**
-     * @brief Atomically increments by one the current value
-     *
-     * @returns the updated value
-     */
-    public long incrementAndGet()
-    {
-        long value = 0;
+		return value;
+	}
 
-        // TODO -- you fill in here
+	/**
+	 * @brief Atomically increments by one the current value
+	 * 
+	 * @returns the updated value
+	 */
+	public long incrementAndGet() {
+		long value = 0;
 
-        return value;
-    }
+		// TODO -- you fill in here
+		mRWLock.writeLock().lock();
+		this.mValue = this.mValue + 1;
+		value = this.mValue;
+		mRWLock.writeLock().unlock();
+
+		return value;
+	}
 }
-
